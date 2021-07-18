3d29dc6be33dc2cf8384337d251a95fd57961edf
TLado <tordaladomerszky@gmail.com>
2021-07-18 16:07:20 +0200

    fixed memory error

--- tsp_approx.py
+++ tsp_approx.py
@@ -1,6 +1,6 @@
 from math import sqrt, floor
 
-def tsp_approx(A: list, n):
+def tsp_approx(coordinates: list, n: int):
     c = 0 # minimal cost TSP tour
     S = {x for x in range(1, n)} # vertices we haven't visited
     u = 0 # first node
@@ -8,29 +8,22 @@ def tsp_approx(A: list, n):
         nn = (float("inf"), None) # distance and index of nearest neighbour
         for v in range(n):
             if v in S:
-                if nn[0] > A[u][v]:
-                    nn = (A[u][v], v)
-                elif nn[0] == A[u][v]:
+                d = compute_dist(u, v, coordinates)
+                if nn[0] > d:
+                    nn = (d, v)
+                elif nn[0] == d:
                     if v < nn[1]:
-                        nn = (A[u][v], v)
+                        nn = (d, v)
         c += nn[0]
         S.remove(nn[1])
         u = nn[1]
-    return floor(c + A[nn[1]][0])
+    return floor(c + compute_dist(u, 0, coordinates))
 
 
-def compute_dist(coordinates, n):
-    rows = [None for _ in range(n)]
-    A = [rows[:] for _ in range(n)] # distances between each pair of points
-    for i in range(n):
-        for j in range(n):
-            if i == j:
-                A[i][j] = float("inf")
-                continue
-            x, y = coordinates[i]
-            z, w = coordinates[j]
-            A[i][j] = sqrt(((x-z)**2)+((y-w)**2)) # Euclidean distance
-    return A
+def compute_dist(u, v, coordinates):
+    x, y = coordinates[u]
+    z, w = coordinates[v]
+    return sqrt(((x-z)**2)+((y-w)**2)) # Euclidean distance
 
 def get_coordinates(file_name: str = "nn.txt"):
     coordinates = []
@@ -43,9 +36,8 @@ def get_coordinates(file_name: str = "nn.txt"):
     return coordinates, n
 
 def main():
-    coordinates, n = get_coordinates()
-    A = compute_dist(coordinates, n)
-    return tsp_approx(A, n) 
+    coordinates, n = get_coordinates("nn_small.txt")
+    return tsp_approx(coordinates, n) 
 
 if __name__ == "__main__":
     print(main())
